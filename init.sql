ce4daf18b89638653dd43c73393d95fb09784ae7
Daniel Arden <dsa99@nau.edu>
2025-11-03 20:10:55 -0700

    Reverting files back to orginal

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
   user_id VARCHAR(255) PRIMARY KEY,
-  password VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   date_registered TIMESTAMP DEFAULT now()
 );
@@ -14,26 +14,17 @@ CREATE TABLE IF NOT EXISTS blogs (
   date_created TIMESTAMP DEFAULT now()
 );
 
-INSERT INTO users (user_id, password, name)
+INSERT INTO users (user_id, password_hash, name)
 VALUES
   ('bc22', 'bobcros', 'Bobby Crosby'),
   ('madbum', 'goingmad12', 'Madison Bumgarner'),
-  ('TheFreak', 'timmyjim', 'Tim Linecum')
+  ('TheFreak', 'timmyjim', 'Tim Lincecum')
 ON CONFLICT (user_id) DO NOTHING;
 
 INSERT INTO blogs (creator_name, creator_user_id, title, body, date_created) VALUES
-('Madison Bumgarner','madbum','World Series WInner','Just beat the royals, Easy win.', now() - interval '10 days'),
-('Bobby Crosby','bc22','Lorem Ipsum or sum','I dont know what to put here. Looks cool though. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellent
-', now() - interval '8 days'),
+('Madison Bumgarner','madbum','World Series Winner','Just beat the royals, Easy win.', now() - interval '10 days'),
+('Bobby Crosby','bc22','Lorem Ipsum','Lorem ipsum dolor sit amet, consectetur adipiscing elit.', now() - interval '8 days'),
 ('Tim Lincecum','TheFreak','GOAT','I got 2 cy youngs. Put me in the HOF.', now() - interval '5 days'),
 ('Tim Lincecum','TheFreak','HOF Ballot','Put Barry Bonds in the hall of fame.', now() - interval '3 days'),
 ('Madison Bumgarner','madbum','HOF','Put Bonds in.', now() - interval '1 days')
 ON CONFLICT DO NOTHING;
-
-DELETE FROM blogs
-WHERE blog_id NOT IN (
-  SELECT MIN(blog_id)
-  FROM blogs
-  GROUP BY title, body, creator_user_id
-);
-
